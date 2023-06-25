9e7990145ae5194a5db582deed00b5f0bf287b79
Andrey Smirnov <andreysmirnov@ingrammicro.com>
2023-06-25 13:53:15 +0300

    initial commit

---
+++ vertx-demo/src/main/java/com/imc/rnd/reactive/vertx/vertxdemo/Main.java
@@ -0,0 +1,9 @@
+package com.imc.rnd.reactive.vertx.vertxdemo;
+
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        out.println("started");
+    }
+}
